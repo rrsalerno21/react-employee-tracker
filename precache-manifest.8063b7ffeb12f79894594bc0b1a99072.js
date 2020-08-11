ea8a5b204d3eb5c1370c083be652861c37f5bfd6
Rocky <rrsalerno21@gmail.com>
2020-08-11 08:09:05 -0700

    Updates

---
+++ precache-manifest.8063b7ffeb12f79894594bc0b1a99072.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f51091c954b8d6c020d629d6f977de1",
+    "url": "/react-employee-tracker/index.html"
+  },
+  {
+    "revision": "32ff1e01c6936fb50d85",
+    "url": "/react-employee-tracker/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "042dcdb895c5aef8adbc",
+    "url": "/react-employee-tracker/static/css/main.dc1376ba.chunk.css"
+  },
+  {
+    "revision": "32ff1e01c6936fb50d85",
+    "url": "/react-employee-tracker/static/js/2.37b47f21.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-employee-tracker/static/js/2.37b47f21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "042dcdb895c5aef8adbc",
+    "url": "/react-employee-tracker/static/js/main.26cab9ad.chunk.js"
+  },
+  {
+    "revision": "30917b4f235debac5242",
+    "url": "/react-employee-tracker/static/js/runtime-main.a932983f.js"
+  }
+]);
