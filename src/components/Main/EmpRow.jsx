9f69e1fc640bdd35568f9b75c5d2aa6faadd7678
Rocky <rrsalerno21@gmail.com>
2020-08-11 08:19:32 -0700

    added alt tag to images

--- src/components/Main/EmpRow.jsx
+++ src/components/Main/EmpRow.jsx
@@ -3,16 +3,15 @@ import { Row, Col } from "react-bootstrap";
 
 const empRow = (props) => {
   const { picture, fName, lName, email, cell } = props;
+  const fullName = `${fName} ${lName}`;
   return (
     <React.Fragment>
       <Row className="border-bottom">
         <Col md lg="2">
-          <img src={picture} />
+          <img src={picture} alt={fullName + " portrait"} />
         </Col>
         <Col md lg="3" className="my-1">
-          <span>
-            {fName} {lName}
-          </span>
+          <span>{fullName}</span>
         </Col>
         <Col md lg="4" className="my-1">
           <span>{email}</span>
